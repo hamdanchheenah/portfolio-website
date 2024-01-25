545ee9dfe9c3bdd1f289ec93773ca8d7b5b48bb2
unknown <m.hamdanchheenah@gmail.com>
2024-01-25 20:51:01 +0500

    first change

---
+++ script.js
@@ -0,0 +1,5 @@
+var typed = new Typed('#element', {
+    strings: ['Frontend developer', 'WEb developer','ux/ui designer','WEb designer','html',
+    'css','javascript','bootstrap','react','everything','at your','doorstep','hire me now!'],
+    typeSpeed: 60,
+  });
